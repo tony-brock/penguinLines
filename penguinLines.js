6448a32db4d353519b4196f0f6cdd92339b0adaa
ajhowell2000 <anazjai@gmail.com>
2020-04-17 19:18:49 -0400

    Newlines

--- penguinLines.js
+++ penguinLines.js
@@ -3,7 +3,7 @@ var success = function(penguins)
     console.log("Data Collected", penguins);
     console.log(penguins[0].quizes[0].day);
  //   console.log(getquizgrades(penguins));
-    lineplot(penguins);
+    console.log(getquizdates(penguins.quizes))
     
 };
 
@@ -15,6 +15,28 @@ var failure = function(error)
 var penguinPromise = d3.json("classData.json");
 penguinPromise.then(success, failure);
 
+var getquizdates= function(quizes){quizes.map(function(dates){
+    return dates.day
+})}
+
+
+var width= "500"
+var height="250"
+
+var svg= d3.select(svg)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", "graph")
+var line= d3.svg.line()
+    .x(function(penguins){return xScale(penguins.quizes.day)})
+        .y(function(penguins){return yScale(penguins.quizes.day)
+        })
+   
+    svg.append("path")
+        .datum(penguin)
+        .attr("class","line")
+        .attr("d",line)
+
 //var getquizgrades = function(penguin)
 //{
 //    var quizgrade=penguin.quizes.map(function(quiz){
@@ -22,34 +44,3 @@ penguinPromise.then(success, failure);
  //   });
 //return quizgrade;
 //}
-
-
-var lineplot= function(penguin){ 
-    
-var height= "500"
-var width= "250"
-var svg= d3.select("svg")
-    .attr("width", width)
-    .attr("height",height)
-    .attr("id", "graph")
-
-    
-  var xScale = d3.scaleLinear()
-                    .domain([d3.min(penguin, function(penguins){return penguins.quizes.day}),
-                            d3.max(penguin,function(penguins){return penguins.quizes.day})])
-                    .range([0, width]);
-    var yScale = d3.scaleLinear()
-                    .domain([d3.min(penguin,function(penguins){return penguins.quizes.grade}),
-                            d3.max(penguin,function(penguins){return penguins.quizes.grade})])
-                    .range([height, 0]);
-    var line= d3.line()
-        .x(function(penguins){return xScale(penguins.quizes.day)})
-        .y(function(penguins){return yScale(penguins.quizes.day)
-        })
-    svg.append("path")
-        .datum(penguin)
-        .attr("class","line")
-        .attr("d",line)
-};
-        
-        
